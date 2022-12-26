9392aab7a516ea07e247566f8355728a83e0ce26
User <>
2022-12-26 03:30:06 +0300

    lesson_1

--- app/src/main/java/com/example/kotlin_task_1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_task_1/MainActivity.kt
@@ -1,14 +1,48 @@
-package com.example.kotlin_task_1;
+package com.example.kotlin_task_1
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-import android.os.Bundle;
+class MainActivity : AppCompatActivity() {
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        val textView: TextView = findViewById(R.id.text_view);
+
+        button.setOnClickListener(View.OnClickListener {
+            textView.setText("TextTextText!");
+        });
+
+        readRepositoryFor()
+        testWhen(ProffessionOfPerson.TEACHER);
     }
+
+
+
+    private fun readRepositoryFor(){
+        for (Person in Repository.personArray){
+            println(Person);
+        }
+    }
+    private fun testWhen(proffessionOfPerson: ProffessionOfPerson) {
+        val proffession = when (proffessionOfPerson) {
+            ProffessionOfPerson.DOCTOR -> "достаем скальпель"
+            ProffessionOfPerson.POLICE -> "виу виу"
+            ProffessionOfPerson.TEACHER -> "протираем доску"
+        }
+       println(proffession);
+    }
+
+
+
+
 }
